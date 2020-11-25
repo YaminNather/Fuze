70a834ba94018310612b7e796761700d578dba88
Yamin Nather <2001s.yn@gmail.com>
2020-11-25 16:41:08 +0530

    Made a BuyBtn Prefab and a BuyBtnMgr class.
    Made a BuyBtn Prefab and a BuyBtnMgr class.

---
+++ Assets/Main Menu/Store Page/Buy Btn/Scripts/BuyBtnMgr.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyBtnMgr : MonoBehaviour
+{ 
+    [SerializeField] private Image m_BallIcon;
+    [SerializeField] private Text m_CurrencyValueLbl;
+    [SerializeField] private Image m_CurrencyIconImage;
+}
